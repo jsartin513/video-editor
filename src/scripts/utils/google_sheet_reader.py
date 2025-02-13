75f526e2027392d914a911694ad979cbea5da935
Jessica Sartin <jessica.sartin@enolink.com>
2025-02-13 18:16:59 -0500

    actually include the right file

--- src/scripts/utils/google_sheet_reader.py
+++ src/scripts/utils/google_sheet_reader.py
@@ -3,6 +3,9 @@ import csv
 from io import StringIO
 
 GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1CC5uA0ZrP39eM6OC0JgE8JlwDrqpr4-ykp6kIKtgHXQ/export?format=csv&gid=182568368"
+COURT_1_HEADER_TEXT = "Court 1"
+COURT_2_HEADER_TEXT = "Court 2"
+COURT_3_HEADER_TEXT = "Court 3"
 
 def get_google_sheet_data():
     response = requests.get(GOOGLE_SHEET_URL)
@@ -14,30 +17,30 @@ def get_google_sheet_data():
 
 def parse_schedule(csv_data):
     reader = csv.DictReader(StringIO(csv_data))
-    schedule = {'Court 1': [], 'Court 2': [], 'Court 3': []}
+    schedule = {COURT_1_HEADER_TEXT: [], COURT_2_HEADER_TEXT: [], COURT_3_HEADER_TEXT: []}
     for (idx, row) in enumerate(reader):
         if idx % 3 == 0: # Home team row
             round_number = row['Round #']
             start_time = row['Start Time']
-            schedule['Court 1'].append({
+            schedule[COURT_1_HEADER_TEXT].append({
                 'round': round_number,
                 'start_time': start_time,
-                'home_team': row['Court 1'],
+                'home_team': row[COURT_1_HEADER_TEXT],
             })
-            schedule['Court 2'].append({
+            schedule[COURT_2_HEADER_TEXT].append({
                 'round': round_number,
                 'start_time': start_time,
-                'home_team': row['Court 2'],
+                'home_team': row[COURT_2_HEADER_TEXT],
             })
-            schedule['Court 3'].append({
+            schedule[COURT_3_HEADER_TEXT].append({
                 'round': round_number,
                 'start_time': start_time,
-                'home_team': row['Court 3'],
+                'home_team': row[COURT_3_HEADER_TEXT],
             })
         elif idx % 3 == 1: # Away team row
-            schedule['Court 1'][-1]['away_team'] = row['Court 1']
-            schedule['Court 2'][-1]['away_team'] = row['Court 2']
-            schedule['Court 3'][-1]['away_team'] = row['Court 3']
+            schedule[COURT_1_HEADER_TEXT][-1]['away_team'] = row[COURT_1_HEADER_TEXT]
+            schedule[COURT_2_HEADER_TEXT][-1]['away_team'] = row[COURT_2_HEADER_TEXT]
+            schedule[COURT_3_HEADER_TEXT][-1]['away_team'] = row[COURT_3_HEADER_TEXT]
     return schedule
 
 
