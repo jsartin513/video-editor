3239e91a7335ee6341cb6c83049a420b716c13f8
Jessica Sartin <jessica.sartin@enolink.com>
2025-02-16 22:50:36 -0500

    first script renames videos and creates metadata, next script processes them

--- src/scripts/process_game_videos.py
+++ src/scripts/process_game_videos.py
@@ -1,23 +1,69 @@
-import argparse
 import json
-import os
-import shutil
-from moviepy.video.VideoClip import *
-from moviepy.video.io.VideoFileClip import VideoFileClip
-from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 from moviepy import *
+import numpy as np
 
-from utils.files import get_video_length, list_files_sorted_by_date
-from utils.google_sheet_reader import get_google_sheet_data, parse_schedule
-
+import argparse
 
 FONT_PATH = "./font/font.ttf"
+
+LOGO_ICON_MAX_WIDTH = 180
+LOGO_ICON_MIN_WIDTH = 120
+TEAM_NAME_MAX_FONT_SIZE = 72
+TEAM_NAME_MIN_FONT_SIZE = 36
+
+STARTING_LOGO_POSITION = (0.11, 0.2)
+STARTING_TEAM_NAME_POSITION = (0.205, 0.25)
+
+ENDING_AWAY_TEAM_LOGO_POSITION = (0.5, 0.8)
+ENDING_HOME_TEAM_LOGO_POSITION = (0.11, 0.8)
+ENDING_AWAY_TEAM_NAME_POSITION = (0.55, 0.8)
+ENDING_HOME_TEAM_NAME_POSITION = (0.205, 0.8)
+
+TOTAL_DURATION = 15
+STANDARD_TRANSITION_TIME = 3
+
 
 def log(message):
     print(message)
 
+
 def format_team_name_for_filename(team_name):
     return team_name.replace(" ", "_").replace(",", "").replace("'", "").lower()
+
+
+
+# Create a circular mask
+def get_circular_mask():
+    center_x, center_y = LOGO_ICON_MAX_WIDTH // 2, LOGO_ICON_MAX_WIDTH // 2
+    width, height = LOGO_ICON_MAX_WIDTH, LOGO_ICON_MAX_WIDTH
+    radius = min(width, height) // 2
+    mask = np.zeros((height, width)) 
+    x, y = np.ogrid[:height, :width]
+    mask[(x - center_y)**2 + (y - center_x)**2 <= radius**2] = 1  
+    return mask
+
+
+
+def function_for_size(t, start_size, end_size, clip_duration=STANDARD_TRANSITION_TIME):
+    return start_size + (end_size - start_size) * t / clip_duration
+
+# Create a function that takes in a time t and returns the position of an object moving from start_position to end_position
+def function_for_position(t, start_position, end_position, clip_duration=STANDARD_TRANSITION_TIME):
+    x_start_position, y_start_position = start_position
+    x_end_position, y_end_position = end_position
+    x_distance = x_end_position - x_start_position
+    y_distance = y_end_position - y_start_position
+
+    return (x_start_position + x_distance * t / clip_duration, y_start_position + y_distance * t / clip_duration)
+
+# This is going to have static information about this tournament
+def get_bdl_tournament_banner():
+    text = "Boston Dodgeball League presents The Throw Down 3 - February 22, 2025"
+    banner = (
+        TextClip(font=FONT_PATH, text=text, size=(1920, 24))
+        .with_position(("center", "top")).with_duration(TOTAL_DURATION)
+    )
+    return banner
 
 # Add the team names to stylized panels in the video
 def add_team_name_to_video(filename, home_team, away_team):
@@ -38,46 +84,8 @@ def add_team_name_to_video(filename, home_team, away_team):
     video_with_text = CompositeVideoClip([video, home_team_clip, away_team_clip])
 
     video_with_text.write_videofile(output_path, codec="libx264", fps=24)
+    return output_path
 
-# add_team_name_to_video("/Users/jessica.sartin/Movies/GoPro/bdl_open_gym_july_22_2024/test_videos/processed_videos/shorter_video.mp4", "team 1", "team 2")
-
-# Create a circular mask
-def get_circular_mask():
-    center_x, center_y = LOGO_ICON_MAX_WIDTH // 2, LOGO_ICON_MAX_WIDTH // 2
-    width, height = LOGO_ICON_MAX_WIDTH, LOGO_ICON_MAX_WIDTH
-    radius = min(width, height) // 2
-    mask = np.zeros((height, width)) 
-    x, y = np.ogrid[:height, :width]
-    mask[(x - center_y)**2 + (y - center_x)**2 <= radius**2] = 1  
-    return mask
-
-LOGO_ICON_MAX_WIDTH = 180
-LOGO_ICON_MIN_WIDTH = 120
-TEAM_NAME_MAX_FONT_SIZE = 72
-TEAM_NAME_MIN_FONT_SIZE = 36
-
-STARTING_LOGO_POSITION = (0.11, 0.2)
-STARTING_TEAM_NAME_POSITION = (0.205, 0.25)
-
-ENDING_AWAY_TEAM_LOGO_POSITION = (0.5, 0.8)
-ENDING_HOME_TEAM_LOGO_POSITION = (0.11, 0.8)
-ENDING_AWAY_TEAM_NAME_POSITION = (0.55, 0.8)
-ENDING_HOME_TEAM_NAME_POSITION = (0.205, 0.8)
-
-TOTAL_DURATION = 15
-STANDARD_TRANSITION_TIME = 3
-
-def function_for_size(t, start_size, end_size, clip_duration=STANDARD_TRANSITION_TIME):
-    return start_size + (end_size - start_size) * t / clip_duration
-
-# Create a function that takes in a time t and returns the position of an object moving from start_position to end_position
-def function_for_position(t, start_position, end_position, clip_duration=STANDARD_TRANSITION_TIME):
-    x_start_position, y_start_position = start_position
-    x_end_position, y_end_position = end_position
-    x_distance = x_end_position - x_start_position
-    y_distance = y_end_position - y_start_position
-
-    return (x_start_position + x_distance * t / clip_duration, y_start_position + y_distance * t / clip_duration)
 
 # Create clips for the team name with "standard" transitions using the variables described above
 def get_name_clips(team_name, start_position, end_position, start_time=0):
@@ -118,16 +126,6 @@ def get_logo_clips(logo_path, ending_logo_position, start_time=0):
         .with_mask(circular_mask).with_position(ending_logo_position, relative=True).with_start(final_position_start)
     )
     return logo_clip_fade_in, logo_clip_moving, logo_clip_final_position
-
-# This is going to have static information about this tournament
-def get_bdl_tournament_banner():
-    text = "Boston Dodgeball League presents The Throw Down 3 - February 22, 2025"
-    banner = (
-        TextClip(font=FONT_PATH, text=text, size=(1920, 24))
-        .with_position(("center", "top")).with_duration(TOTAL_DURATION)
-    )
-    return banner
-
 # Create opening screen with "standard" transitions using the variables described above
 def create_opening_screen(output_directory, game):
     home_team = game["home_team"]
@@ -162,104 +160,32 @@ def create_opening_screen(output_directory, game):
     ])
     opening_screen.write_videofile(output_path, codec="libx264", fps=24)
 
-# create_opening_screen("Kids Next Door", "Boston T Titans")
-
-# Rename the videos in the directory to the following format: "Home Team vs. Away Team.mp4"
-# directory_name: the name of the directory containing the videos
-# ordered_teams_including_refs: a list of teams in the order they appear in the video
-def rename_videos(directory_name, games_list):
-    output_directory = f'{directory_name}/processed_videos'
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-
-    videos = get_likely_ordered_game_filenames(directory_name)
-    video_paths = []
-    for idx, video in enumerate(videos):
-        game = games_list[idx]
-        home_team = game["home_team"]
-        away_team = game["away_team"]
-        round = game["round"]
-        new_video_name = f"{format_team_name_for_filename(home_team)}_{format_team_name_for_filename(away_team)}_round_robin_round_{round}.mp4"
-        new_video_path = os.path.join(output_directory, new_video_name)
-        old_video_path = os.path.join(directory_name, video)
-        shutil.copy(old_video_path, new_video_path)
-        log(f"Copied {video} to {new_video_path}")
-        game["video_path"] = new_video_path
-        video_paths.append(new_video_path)
-    return output_directory, video_paths
-
-
-# Pull all videos in the directory greater than five minutes long
-def get_likely_ordered_game_filenames(directory_name):
-    game_video_filenames = []
-    for video in list_files_sorted_by_date(directory_name):
-        if video.endswith(".mp4"):
-            video_path = os.path.join(directory_name, video)
-            video_length = get_video_length(video_path)
-            if video_length >= 300:
-                game_video_filenames.append(video)
-    log(f"Game video filenames: {game_video_filenames}")
-    return game_video_filenames
-
-
-def create_metadata_file(schedule, output_path, video_paths):
-    metadata = []
-    for idx, game in enumerate(schedule):
-        metadata.append({
-            "home_team": game["home_team"],
-            "away_team": game["away_team"],
-            "video_path": video_paths[idx],
-            "home_team_score": game.get("home_team_score", None),
-            "away_team_score": game.get("away_team_score", None),
-            "round": game["round"],
-            "start_time": game["start_time"],
-            "home_team_logo_path": game.get("home_team_logo_path", None),
-            "away_team_logo_path": game.get("away_team_logo_path", None)
-        })
-    with open(f"{output_path}/metadata.json", "w") as f:
-        json.dump(metadata, f)
-    log(f"Metadata file created at {output_path}/metadata.json")
-
-def organize_video_files(directory_name, ordered_games):
-    # Rename the videos in the directory
-    output_path, video_paths = rename_videos(directory_name, ordered_games)
-    create_metadata_file(ordered_games, output_path, video_paths)
 
 def process_game(output_path, game):
     create_opening_screen(output_path, game)
     # add_team_name_to_video(game["video_path"], game["home_team"], game["away_team"])
 
-    
-    
+
+def run(output_path, games):
+    for game in games:
+        log(f"Processing game at {game['video_path']}")
+        process_game(output_path, game)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Handle tournament videos.')
     parser.add_argument('directory_name', type=str, help='The name of the directory containing the videos')
-    parser.add_argument('--court', type=int, help='The court number')
-    # parser.add_argument('--round_type', type=str, help='The type of round (round_robin, playoffs, finals)', default='round_robin')
-    # parser.add_argument('--min_video_length', type=int, help='The minimum length of a video in seconds', default=300)
+
+
     args = parser.parse_args()
 
-    # directory_name = args.directory_name
-    sheet_data = get_google_sheet_data()
-    schedule = parse_schedule(sheet_data)
-
-    # for court_number in range(1, 4):
-    court_name = f"Court {args.court}"
-    ordered_games = schedule[court_name]
-
-    # video_paths =rename_videos(args.directory_name, ordered_games)
     output_path = f"{args.directory_name}/processed_videos"
+    metadata_path = f"{output_path}/metadata.json"
 
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    with open(metadata_path, "r") as f:
+        games = json.load(f)
 
-    opening_screens_output_path = f"{output_path}/opening_screens"
-    # for game in ordered_games:
-    #     create_opening_screen(output_path, game)
-    # create_opening_screen(output_path, ordered_games[0])
-    
-    # log(f"Ordered teams: {ordered_teams}")
 
-    # organize_video_files(args.directory_name, ordered_games)
-    process_game(output_path, ordered_games[0]) # next steps: separate these scripts and read this from the metadata file
+    # Just run on one game for testing
+    games = games[:1]
+    run(output_path, games)
