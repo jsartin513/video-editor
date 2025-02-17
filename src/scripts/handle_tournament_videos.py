0bb3ae9210b43af4c42245633c42646d7db13bbe
Jessica Sartin <jessica.sartin@enolink.com>
2025-02-16 21:44:23 -0500

    Create metadata file

--- src/scripts/handle_tournament_videos.py
+++ src/scripts/handle_tournament_videos.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import shutil
 from moviepy.video.VideoClip import *
@@ -177,14 +178,15 @@ def rename_videos(directory_name, games_list):
         game = games_list[idx]
         home_team = game["home_team"]
         away_team = game["away_team"]
-        new_video_name = f"{home_team}_{away_team}_round_robin.mp4"
+        round = game["round"]
+        new_video_name = f"{format_team_name_for_filename(home_team)}_{format_team_name_for_filename(away_team)}_round_robin_round_{round}.mp4"
         new_video_path = os.path.join(output_directory, new_video_name)
         old_video_path = os.path.join(directory_name, video)
         shutil.copy(old_video_path, new_video_path)
         log(f"Copied {video} to {new_video_path}")
         game["video_path"] = new_video_path
         video_paths.append(new_video_path)
-    return video_paths
+    return output_directory, video_paths
 
 
 # Pull all videos in the directory greater than five minutes long
@@ -200,9 +202,28 @@ def get_likely_ordered_game_filenames(directory_name):
     return game_video_filenames
 
 
-def run(directory_name, ordered_teams_including_refs):
+def create_metadata_file(schedule, output_path, video_paths):
+    metadata = []
+    for idx, game in enumerate(schedule):
+        metadata.append({
+            "home_team": game["home_team"],
+            "away_team": game["away_team"],
+            "video_path": video_paths[idx],
+            "home_team_score": game.get("home_team_score", None),
+            "away_team_score": game.get("away_team_score", None),
+            "round": game["round"],
+            "start_time": game["start_time"],
+            "home_team_logo_path": game.get("home_team_logo_path", None),
+            "away_team_logo_path": game.get("away_team_logo_path", None)
+        })
+    with open(f"{output_path}/metadata.json", "w") as f:
+        json.dump(metadata, f)
+    log(f"Metadata file created at {output_path}/metadata.json")
+
+def run(directory_name, ordered_games):
     # Rename the videos in the directory
-    rename_videos(directory_name, ordered_teams_including_refs)
+    output_path, video_paths = rename_videos(directory_name, ordered_games)
+    create_metadata_file(ordered_games, output_path, video_paths)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Handle tournament videos.')
@@ -221,16 +242,16 @@ if __name__ == '__main__':
     ordered_games = schedule[court_name]
 
     # video_paths =rename_videos(args.directory_name, ordered_games)
-    output_path = f"{args.directory_name}/processed_videos/opening_screens"
+    output_path = f"{args.directory_name}/processed_videos"
 
     # if not os.path.exists(output_path):
     #     os.makedirs(output_path)
 
-
+    opening_screens_output_path = f"{output_path}/opening_screens"
     # for game in ordered_games:
     #     create_opening_screen(output_path, game)
-    create_opening_screen(output_path, ordered_games[0])
+    # create_opening_screen(output_path, ordered_games[0])
     
     # log(f"Ordered teams: {ordered_teams}")
 
-    # run(directory_name, ordered_teams)
+    run(args.directory_name, ordered_games)
