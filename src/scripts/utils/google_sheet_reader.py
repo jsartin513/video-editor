233e48709b4cfa4df0ec910152809620d43bbcf6
Jess Sartin <jsartin513@gmail.com>
2025-02-24 11:07:02 -0500

    add scores to metadata

--- src/scripts/utils/google_sheet_reader.py
+++ src/scripts/utils/google_sheet_reader.py
@@ -6,6 +6,9 @@ GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1CC5uA0ZrP39eM6OC0JgE
 COURT_1_HEADER_TEXT = "Court 1"
 COURT_2_HEADER_TEXT = "Court 2"
 COURT_3_HEADER_TEXT = "Court 3"
+COURT_1_SCORE_TEXT = "Score_1"
+COURT_2_SCORE_TEXT = "Score_2"
+COURT_3_SCORE_TEXT = "Score_3"
 
 
 def get_logo_path(team_name):
@@ -31,16 +34,20 @@ def parse_schedule(csv_data, add_logo_paths=True):
                 'round': round_number,
                 'start_time': start_time,
                 'home_team': row[COURT_1_HEADER_TEXT],
+                'home_team_score': row[COURT_1_SCORE_TEXT],
             })
             schedule[COURT_2_HEADER_TEXT].append({
                 'round': round_number,
                 'start_time': start_time,
                 'home_team': row[COURT_2_HEADER_TEXT],
+                'home_team_score': row[COURT_2_SCORE_TEXT],
             })
             schedule[COURT_3_HEADER_TEXT].append({
                 'round': round_number,
                 'start_time': start_time,
                 'home_team': row[COURT_3_HEADER_TEXT],
+                'home_team_score': row[COURT_3_SCORE_TEXT],
+                
             })
             if add_logo_paths:
                 schedule[COURT_1_HEADER_TEXT][-1]['home_team_logo_path'] = get_logo_path(row[COURT_1_HEADER_TEXT])
@@ -48,8 +55,11 @@ def parse_schedule(csv_data, add_logo_paths=True):
                 schedule[COURT_3_HEADER_TEXT][-1]['home_team_logo_path'] = get_logo_path(row[COURT_3_HEADER_TEXT])
         elif idx % 3 == 1: # Away team row
             schedule[COURT_1_HEADER_TEXT][-1]['away_team'] = row[COURT_1_HEADER_TEXT]
+            schedule[COURT_1_HEADER_TEXT][-1]['away_team_score'] = row[COURT_1_SCORE_TEXT]
             schedule[COURT_2_HEADER_TEXT][-1]['away_team'] = row[COURT_2_HEADER_TEXT]
+            schedule[COURT_2_HEADER_TEXT][-1]['away_team_score'] = row[COURT_2_SCORE_TEXT]
             schedule[COURT_3_HEADER_TEXT][-1]['away_team'] = row[COURT_3_HEADER_TEXT]
+            schedule[COURT_3_HEADER_TEXT][-1]['away_team_score'] = row[COURT_3_SCORE_TEXT]
             if add_logo_paths:
                 schedule[COURT_1_HEADER_TEXT][-1]['away_team_logo_path'] = get_logo_path(row[COURT_1_HEADER_TEXT])
                 schedule[COURT_2_HEADER_TEXT][-1]['away_team_logo_path'] = get_logo_path(row[COURT_2_HEADER_TEXT])
