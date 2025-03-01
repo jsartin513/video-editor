22e84b5a6638442d76f36335064a0bcc21114c95
Jessica Sartin <jessica.sartin@enolink.com>
2025-03-01 17:19:06 -0500

    Handle bracket thru semis

--- src/scripts/utils/google_sheet_reader.py
+++ src/scripts/utils/google_sheet_reader.py
@@ -3,13 +3,16 @@ import csv
 from io import StringIO
 
 ROUND_ROBIN_SHEET_URL = "https://docs.google.com/spreadsheets/d/1CC5uA0ZrP39eM6OC0JgE8JlwDrqpr4-ykp6kIKtgHXQ/export?format=csv&gid=182568368"
-BRACKET_SHEET_URL = "https://docs.google.com/spreadsheets/d/1CC5uA0ZrP39eM6OC0JgE8JlwDrqpr4-ykp6kIKtgHXQ/edit?gid=2111325620#gid=2111325620"
+BRACKET_SHEET_URL = "https://docs.google.com/spreadsheets/d/1CC5uA0ZrP39eM6OC0JgE8JlwDrqpr4-ykp6kIKtgHXQ/export?format=csv&gid=2111325620"
 COURT_1_HEADER_TEXT = "Court 1"
 COURT_2_HEADER_TEXT = "Court 2"
 COURT_3_HEADER_TEXT = "Court 3"
 COURT_1_SCORE_TEXT = "Score_1"
 COURT_2_SCORE_TEXT = "Score_2"
 COURT_3_SCORE_TEXT = "Score_3"
+
+BRACKET_ROUND_COLUMNS = ["Round 1", "Quarters", "Semis"]
+FINALS_ROUND_COLUMNS = ["Finals", "Championship"]
 
 
 def get_logo_path(team_name):
@@ -28,6 +31,43 @@ def get_google_sheet_data(sheet_url):
     response.raise_for_status()  # Ensure we notice bad responses
     csv_data = response.text
     return csv_data
+
+def get_parsed_bracket():
+    sheet_data = get_google_sheet_data(BRACKET_SHEET_URL)
+    bracket = parse_bracket(sheet_data)
+    return bracket
+
+
+def parse_bracket(csv_data):
+    csv_data = get_google_sheet_data(BRACKET_SHEET_URL)
+    reader = csv.DictReader(StringIO(csv_data))
+    bracket = {round_name: [] for round_name in BRACKET_ROUND_COLUMNS}
+    finals = {round_name: [] for round_name in FINALS_ROUND_COLUMNS}
+    for row in reader:
+        for round_name in BRACKET_ROUND_COLUMNS:
+            if row.get(round_name) and row[round_name].strip() != "":
+                bracket[round_name].append(row[round_name])
+        for round_name in FINALS_ROUND_COLUMNS:
+            if row.get(round_name)and row[round_name].strip() != "":
+                finals[round_name].append(row[round_name])
+    
+    games = []
+    for round_name, round_games in bracket.items():
+        for idx, game_piece in enumerate(round_games):
+            if idx % 3 == 0:
+                try:
+                        game = {
+                            "round": round_name,
+                            "home_team": round_games[idx],
+                            "court": round_games[idx + 1],
+                            "away_team": round_games[idx + 2]
+                        }
+                        games.append(game)
+                except IndexError:
+                    print(f"IndexError: {round_name}, {round_games}")
+                    pass
+    return games
+
 
 
 def parse_schedule(csv_data, add_logo_paths=True):
